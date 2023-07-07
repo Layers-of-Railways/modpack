a0a8c34d33cca251ad6532b7d254bb4915fd7e53
IThundxr <contact@ithundxr.dev>
2023-07-07 14:31:23 -0400

    boop bye bye mods >:3
    Signed-off-by: IThundxr <contact@ithundxr.dev>

--- kubejs/server_scripts/recipe_removals.js
+++ kubejs/server_scripts/recipe_removals.js
@@ -5,14 +5,9 @@ ServerEvents.recipes((event) => {
     let removerecipeinput = [];
 
     let removerecipeoutput = [
-        "create_confectionery:full_chocolate_bar",
-        "create_confectionery:full_black_chocolate_bar",
-        "create_confectionery:full_white_chocolate_bar",
-        "create_confectionery:full_ruby_chocolate_bar",
         "advancedperipherals:chunk_controller",
         "advancedperipherals:ar_controller",
         "advancedperipherals:ar_goggles",
-        "simpleshops:simple_shop",
     ];
 
     removerecipe.forEach((recipeid) => {
