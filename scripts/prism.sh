c04069e8048a5bfb6b66a75755f7c0dedec0b6d7
IThundxr <me@ithundxr.dev>
2024-03-26 21:01:30 -0400

    Cleanup unsup.ini and add colors
    Signed-off-by: IThundxr <me@ithundxr.dev>

--- scripts/prism.sh
+++ scripts/prism.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Create output folder
 mkdir -p output/
+
+# Copy unsup.ini to prism/.minecraft
+cp unsup.ini ./prism/.minecraft/
+
+# cd to prism folder
 cd prism/ || exit
+
+# Zip it up and place it in output folder
 zip -r ../output/RailwaysPrismPack.zip .
+
+# Delete unsup.ini file
+rm ./.minecraft/unsup.ini
