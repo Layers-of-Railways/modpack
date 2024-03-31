b5b15136063ad04d4b314951a737a552cddcc512
IThundxr <me@ithundxr.dev>
2024-03-31 10:35:00 -0400

    update scripts
    Signed-off-by: IThundxr <me@ithundxr.dev>

--- scripts/prism.sh
+++ scripts/prism.sh
@@ -8,7 +8,6 @@ mkdir -p prism/.minecraft/
 
 # Copy unsup.ini & unsup.jar to prism/.minecraft
 cp unsup.ini prism/.minecraft/
-cp unsup.jar prism/.minecraft/
 
 # cd to prism folder
 cd prism/ || exit
@@ -16,5 +15,5 @@ cd prism/ || exit
 # Zip it up and place it in output folder
 zip -r ../output/RailwaysPrismPack.zip .
 
-# Delete unsup.ini & unsup.jar files
-rm -rf .minecraft/
+# Delete unsup.ini
+rm .minecraft/ unsup.ini
