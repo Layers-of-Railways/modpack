24114eb50ffb2d88d35263dacd7f3b879a71055f
MythicHH <112460128+MythicHH@users.noreply.github.com>
2024-04-19 13:53:47 +0800

    Update tags.js

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -1,4 +1,5 @@
 ServerEvents.tags("fluid", (event) => {
     // Bottomless Honey
     event.add("create:bottomless/allow", "create:honey");
+    event.add("create:bottomless/allow", "minecraft:milk");
 });
