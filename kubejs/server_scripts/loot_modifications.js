08df57d2421923f1e53b34aa9d253e5172e2de26
IThundxr <me@ithundxr.dev>
2024-06-18 22:12:14 -0400

    loot modifications
    Signed-off-by: IThundxr <me@ithundxr.dev>

--- kubejs/server_scripts/loot_modifications.js
+++ kubejs/server_scripts/loot_modifications.js
@@ -1,4 +1,7 @@
+let removeFromAllLootTables = ["minecraft:totem"];
+
 onEvent("lootjs", (event) => {
-    // disable totems
-    event.addBlockLootModifier(/.*/).removeLoot("minecraft:totem");
+    removeFromAllLootTables.forEach((item) => {
+        event.addBlockLootModifier(/.*/).removeLoot(item);
+    });
 });
