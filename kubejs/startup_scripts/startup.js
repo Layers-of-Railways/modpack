256b85ba5057a262b61f384594db0632c5ffbf7a
Violet-Scarelli <alphaone150@gmail.com>
2024-10-15 14:18:37 -0700

    Startup script changes + an asset or three

---
+++ kubejs/startup_scripts/startup.js
@@ -0,0 +1,7 @@
+StartupEvents.registry('item', e => {
+
+    e.create('voidgazer_spyglass').displayName('Voidgazer Spyglass').texture('relics:item/voidgazer_spyglass').unstackable().rarity('epic').glow(true);
+    e.create('antenna').displayName('Antenna').texture('phonos:item/antenna');
+    e.create('satellite_dish').displayName('Satellite Dish').texture('phonos:item/dish_antenna');
+    
+  })
