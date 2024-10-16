ddd0eaefaac09e9f5b6ff07cb860d1289a43e50a
Violet-Scarelli <alphaone150@gmail.com>
2024-10-16 15:12:06 -0700

    Testing an item thing

--- kubejs/startup_scripts/startup.js
+++ kubejs/startup_scripts/startup.js
@@ -1,6 +1,24 @@
 StartupEvents.registry('item', e => {
 
-    e.create('voidgazer_spyglass').displayName('Voidgazer Spyglass').texture('relics:item/voidgazer_spyglass').unstackable().rarity('epic').glow(true);
+    e.create('voidgazer_spyglass').displayName('Voidgazer Spyglass').texture('relics:item/voidgazer_spyglass').unstackable().rarity('epic').glow(true).useAnimation('bow')
+    .useDuration(itemstack => 72000)
+    .use(level, player, hand => {
+      player.playSound("minecraft:block.respawn_anchor.charge", 5, 0);
+      return true;
+    })
+    .releaseUsing((itemstack, level, entity, tick) => {
+      var destination = entity.rayTrace(128, false);
+      var destX = destination.getHitX();
+      var destY = destination.getHitY();
+      var destZ = destination.getHitZ();
+      var offhandItem = entity.getOffHandItem();
+      var offhandCount = entity.offHandItem.getCount();
+      if (offhandItem.getId() == `minecraft:amethyst_shard` && offhandCount > 0 && destination.block != null){
+        entity.offHandItem.setCount(offhandCount - 1);
+        entity.moveTo(destX, destY, destZ);
+        entity.playSound("minecraft:entity.warden.sonic_boom", 5, 1);
+      }
+    });
     e.create('antenna').displayName('Antenna').texture('phonos:item/antenna');
     e.create('satellite_dish').displayName('Satellite Dish').texture('phonos:item/dish_antenna');
     
