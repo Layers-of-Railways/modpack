256180d2e74c48a74d30def3d68449e18cd686dd
Violet-Scarelli <alphaone150@gmail.com>
2024-10-16 20:29:03 -0700

    More work

--- kubejs/startup_scripts/startup.js
+++ kubejs/startup_scripts/startup.js
@@ -12,20 +12,24 @@ StartupEvents.registry('item', e => {
       player.playSound("minecraft:block.respawn_anchor.charge", 5, 0);
       return true;
     })
-    .releaseUsing((_itemstack, level, entity, _tick) => {
+    .releaseUsing((_itemstack, _level, entity, _tick) => {
       var destination = entity.rayTrace(64, false);
       var destX = destination.getHitX();
       var destY = destination.getHitY();
       var destZ = destination.getHitZ();
+      var fuelNeeded = Math.ceil(entity.getDistance(destX, destY, destZ));
       var offhandItem = entity.getOffHandItem();
       var offhandCount = entity.offHandItem.getCount();
-      if (offhandItem.getId() == `minecraft:amethyst_shard` && offhandCount > 0 && destination.block != null){
-        entity.offHandItem.setCount(offhandCount - destination.distance);
+      if (offhandItem.getId() == `minecraft:amethyst_shard` && offhandCount > 0 && destination.block != null && fuelNeeded < offhandCount){
+        entity.offHandItem.setCount(offhandCount - fuelNeeded);
         entity.teleportTo(destX, destY, destZ);
         entity.playSound("minecraft:entity.warden.sonic_boom", 5, 1);
+      }
+      else{
+        entity.attack(4);
+        entity.playSound("minecraft:block.respawn_anchor.deplete", 5, 1);
       }
     });
     e.create('antenna').displayName('Antenna').texture('phonos:item/antenna');
     e.create('satellite_dish').displayName('Satellite Dish').texture('phonos:item/dish_antenna');
-    
   })
