87cb999e499e0fe37b20c49c0faa38b6e098fc8c
IThundxr <me@ithundxr.dev>
2024-11-16 18:25:27 -0500

    Reformat js scripts & Merge #36

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -8,8 +8,8 @@ let better_end_chests = [
     "betterend:helix_tree_chest",
     "betterend:umbrella_tree_chest",
     "betterend:jellyshroom_chest",
-    "betterend:lucernia_chest"
-]
+    "betterend:lucernia_chest",
+];
 
 let better_end_barrels = [
     "betterend:mossy_glowshroom_barrel",
@@ -21,8 +21,22 @@ let better_end_barrels = [
     "betterend:helix_tree_barrel",
     "betterend:umbrella_tree_barrel",
     "betterend:jellyshroom_barrel",
-    "betterend:lucernia_barrel"
-]
+    "betterend:lucernia_barrel",
+];
+
+let decorative_blocks_palisades = [
+    "decorative_blocks:oak_palisade",
+    "decorative_blocks:birch_palisade",
+    "decorative_blocks:spruce_palisade",
+    "decorative_blocks:acacia_palisade",
+    "decorative_blocks:jungle_palisade",
+    "decorative_blocks:dark_oak_palisade",
+    "decorative_blocks:mangrove_palisade",
+    "decorative_blocks:cherry_palisade",
+    "decorative_blocks:bamboo_palisade",
+    "decorative_blocks:crimson_palisade",
+    "decorative_blocks:warped_palisade",
+];
 
 let non_movable = [
     "armourers_workshop:skin-library-creative",
@@ -30,29 +44,30 @@ let non_movable = [
     "create:creative_fluid_tank",
     "create:creative_crate",
     "numismatics:creative_vendor",
-    "createaddition:creative_energy"
-]
+    "createaddition:creative_energy",
+];
 
-let bottomless_allow = [
-    "create:honey",
-    "minecraft:milk"
-]
+let bottomless_allow = ["create:honey", "minecraft:milk"];
 
 ServerEvents.tags("block", (event) => {
     better_end_chests.forEach((id) => {
         event.add("lootr:convert/chests", id);
-    })
+    });
     better_end_barrels.forEach((id) => {
         event.add("lootr:convert/barrels", id);
-    })
+    });
+
+    decorative_blocks_palisades.forEach((id) => {
+        event.add("railways:semaphore_poles", id);
+    });
 
     non_movable.forEach((id) => {
         event.add("create:non_movable", id);
-    })
+    });
 });
 
 ServerEvents.tags("fluid", (event) => {
     bottomless_allow.forEach((id) => {
         event.add("create:bottomless/allow", id);
-    })
+    });
 });
