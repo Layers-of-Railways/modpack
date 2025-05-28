92ea54abd065349f187ac5936d07c832370822a4
Kirin <fletcher1337@gmail.com>
2025-05-28 20:22:42 +1200

    Update biofuel_burn_time.js
    Removed rarity

--- kubejs/startup_scripts/biofuel_burn_time.js
+++ kubejs/startup_scripts/biofuel_burn_time.js
@@ -1,10 +1,8 @@
 ItemEvents.modification(event => {
     event.modify('createaddition:bioethanol_bucket', item => {
-        item.rarity = 'RARE'
         item.burnTime = 20000
     })
     event.modify('createaddition:seed_oil_bucket', item => {
-        item.rarity = 'RARE'
         item.burnTime = 16000
   })
 })
