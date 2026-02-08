8d1aacea836ea4b64e76484783692e5784688684
破笔 <79563897+TheBrokenPen@users.noreply.github.com>
2026-02-08 16:22:57 +0800

    Create splashing.js
    added  consistency concrete spalshing recipe

---
+++ kubejs/server_scripts/recipes/create/splashing.js
@@ -0,0 +1,16 @@
+ServerEvents.recipes((event) => {
+    // Dirt Splashing
+    event.custom({
+      "type": "create:splashing",
+      "ingredients": [
+        {
+          "item": "consistency_plus:concrete_powder"
+        }
+      ],
+      "results": [
+        {
+          "item": "consistency_plus:concrete"
+        }
+      ]
+    });
+  
